467786d854172eff2e833afc89a127aa88ce5f61
Brennan Abanades <spamspambrennan@gmail.com>
2021-07-01 11:38:04 +0100

    Added option for command line usage.

---
+++ ABlooper/command_line.py
@@ -0,0 +1,24 @@
+import argparse
+from ABlooper.ABlooper import CDR_Predictor
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file_path", help="Path to the IMGT numbered antibody pdb file for which the CDRs are to be "
+                                      "remodelled")
+parser.add_argument("-o", "--output", help="Path to where the output model should be saved. Defaults to the same "
+                                           "directory as input file.", default="None")
+parser.add_argument("-H", "--heavy_chain", help="Heavy chain ID for input file. (Default is H)", default="H")
+parser.add_argument("-L", "--light_chain", help="Light chain ID for input file. (Default is L)", default="L")
+parser.add_argument("--confidence_score", help="Print confidence score for each loop", default="False",
+                    action="store_true")
+
+args = parser.parse_args()
+
+
+def main():
+    predictor = CDR_Predictor(args.file_path, chains=(args.heavy_chain, args.light_chain))
+    output_file = args.output
+    if output_file is None:
+        output_file = args.file[:-4] + "_remodelled_CDRs.pdb"
+    predictor.write_predictions_in_pdb_format(output_file)
+    if args.confidence_score:
+        print(predictor.decoy_diversity)
--- setup.py
+++ setup.py
@@ -9,9 +9,10 @@ setup(
     maintainer_email='brennan.abanadeskenyon@stx.ox.ac.uk',
     include_package_data=True,
     packages=find_packages(include=('ABlooper', 'ABlooper.*')),
+    entry_points={'console_scripts': ['ABlooper=ABlooper.command_line:main']},
     install_requires=[
         'numpy',
         'einops>=0.3',
         'torch>=1.6',
-        ],
+    ],
 )
