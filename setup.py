9c03465962cddb4362de58a559d592935740242b
Brennan Abanades <spamspambrennan@gmail.com>
2021-07-27 14:53:26 +0100

    Added code to pypi for easier installation

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 setup(
     name='ABlooper',
-    version='0.0.1',
+    version='1.0.1',
     description='Set of functions to predict CDR structure',
     license='BSD 3-clause license',
     maintainer='Brennan Abanades',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     maintainer_email='brennan.abanadeskenyon@stx.ox.ac.uk',
     include_package_data=True,
     packages=find_packages(include=('ABlooper', 'ABlooper.*')),
